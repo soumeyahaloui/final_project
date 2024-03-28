5a3e7f0cce5cea1256ff50a918b4b7d13b7f9fcd
هند الشقرفى <123508736+ELHEND@users.noreply.github.com>
2024-03-28 21:38:10 +0000

    Initial commit: Added BeneficiaryScreen, SignUpScreen, LoginScreen, ProfileScreen, CustomMDTextField, SomeScreen, and MyApp classes.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # main.py
 from kivy.config import Config
 
-# Set te size to common Android phone screen size
+# Set e size to common Android phone screen size
 Config.set('graphics', 'width', '360')  # 1080 / 3
 Config.set('graphics', 'height', '640')  # 1920 / 3
 Config.write()
